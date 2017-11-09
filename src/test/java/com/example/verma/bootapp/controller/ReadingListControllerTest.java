77a92be1a56e619fd0b3ec78799738877e734b9f
Sanjit Verma <SANJIT@SANJITs-MacBook-Air.local>
2017-11-09 20:39:50 +0530

    added integration test

--- src/test/java/com/example/verma/bootapp/controller/ReadingListControllerTest.java
+++ src/test/java/com/example/verma/bootapp/controller/ReadingListControllerTest.java
@@ -1,24 +1,34 @@
 package com.example.verma.bootapp.controller;
 
 import com.example.verma.bootapp.BootAppApplication;
+import com.example.verma.bootapp.config.SecurityConfig;
+import com.example.verma.bootapp.dto.Book;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.SpringBootConfiguration;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
+import org.springframework.http.MediaType;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.security.test.context.support.WithUserDetails;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
+import static org.hamcrest.Matchers.*;
 import static org.hamcrest.collection.IsEmptyCollection.empty;
 import static org.hamcrest.core.Is.is;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.httpBasic;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.user;
+import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 /**
  * Created by SANJIT on 06/11/17.
@@ -36,24 +46,48 @@ public class ReadingListControllerTest {
 
 
     @Before
-    public void setupMockMVC(){
+    public void setupMockMVC() {
         mockMvc = MockMvcBuilders
                 .webAppContextSetup(webApplicationContext)
+                .apply(springSecurity())
                 .build();
     }
 
 
     @Test
+    @WithMockUser(username = "SANJIT", password = "SANJIT", roles = "USER")
     public void testgetReadingList() throws Exception {
         mockMvc.perform(get("/sanjit"))
-        .andExpect(status().isOk())
-        .andExpect(model().attributeExists("books"))
-        .andExpect(model().attribute("books",is(empty())))
-        .andExpect(view().name("readingList"));
+                .andExpect(status().isOk())
+                .andExpect(model().attributeExists("books"))
+                .andExpect(model().attribute("books", is(empty())))
+                .andExpect(view().name("readingList"));
     }
 
     @Test
-    public void testaddBooktoReadingList(){
-        
+    @WithMockUser(username = "SANJIT", password = "SANJIT", roles = "USER")
+    public void testaddBooktoReadingList() throws Exception {
+        Book expectedBook = new Book();
+        expectedBook.setId(1L);
+        expectedBook.setReader("sanjit");
+        expectedBook.setTitle("BOOK TITLE");
+        expectedBook.setAuthor("BOOK AUTHOR");
+        expectedBook.setIsbn("1234567890");
+        expectedBook.setDescription("DESCRIPTION");
+        mockMvc.perform(post("/sanjit").with(user("sanjit").password("pwd").roles("USER"))
+                .contentType(MediaType.APPLICATION_FORM_URLENCODED)
+                .param("title", "BOOK TITLE")
+                .param("author", "BOOK AUTHOR")
+                .param("isbn", "1234567890")
+                .param("description", "DESCRIPTION"))
+                .andExpect(status().is3xxRedirection())
+                .andExpect(header().string("Location", "/sanjit"));
+
+        mockMvc.perform(get("/sanjit"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("readingList"))
+                .andExpect(model().attributeExists("books"))
+                .andExpect(model().attribute("books", hasSize(1)))
+                .andExpect(model().attribute("books", contains(samePropertyValuesAs(expectedBook))));
     }
 }
---
+++ src/test/java/com/example/verma/bootapp/controller/ServerWebTest.java
@@ -0,0 +1,68 @@
+package com.example.verma.bootapp.controller;
+
+import com.example.verma.bootapp.BootAppApplication;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import java.io.File;
+import java.util.concurrent.TimeUnit;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * Created by SANJIT on 09/11/17.
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringBootTest(classes = BootAppApplication.class,
+        webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class ServerWebTest {
+
+    private static ChromeDriver chromeDriver;
+
+    @Value("${local.server.port}")
+    private int port;
+
+    @BeforeClass
+    public static void openBrowser() {
+
+        final File file = new File("/Users/SANJIT/boot-app/src/test/java/resources/chromedriver");
+        System.setProperty("webdriver.chrome.driver", file.getAbsolutePath());
+        chromeDriver = new ChromeDriver();
+        chromeDriver.manage().timeouts().implicitlyWait(1000, TimeUnit.SECONDS);
+    }
+
+    @Test
+    public void testaddBookToList() {
+        String baseURL = "http://localhost:" + port + "/sanjit";
+        chromeDriver.get(baseURL);
+        assertEquals("You have no books in your book list",
+                chromeDriver.findElementByTagName("div").getText());
+        chromeDriver.findElementByName("title").sendKeys("BOOK TITLE");
+        chromeDriver.findElementByName("author")
+                .sendKeys("BOOK AUTHOR");
+        chromeDriver.findElementByName("isbn")
+                .sendKeys("1234567890");
+        chromeDriver.findElementByName("description")
+                .sendKeys("DESCRIPTION");
+        chromeDriver.findElementByTagName("form")
+                .submit();
+
+        WebElement dl = chromeDriver.findElementByCssSelector("dt.bookHeadline");
+        assertEquals("BOOK TITLE by BOOK AUTHOR (ISBN: 1234567890)", dl.getText());
+        WebElement dt = chromeDriver.findElementByCssSelector("dd.bookDescription");
+        assertEquals("DESCRIPTION", dt.getText());
+    }
+
+    @AfterClass
+    public static void closeBrowser() {
+        chromeDriver.quit();
+    }
+
+}
